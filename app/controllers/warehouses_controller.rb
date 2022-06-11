9d7ca9e64c950235e3fb7b4302d6514c27261e1a
Vanessa <machado.vanessa32@gmail.com>
2022-06-11 14:44:37 -0300

    Galpão cadastrado e exibindo mensagem de sucesso

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,22 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name,
+                                                        :code,
+                                                        :city, 
+                                                        :description, 
+                                                        :address, 
+                                                        :cep, 
+                                                        :area
+                                                      )
+    warehouse = Warehouse.new(warehouse_params)
+    warehouse.save()
+    redirect_to root_path, notice: 'Galpão cadastrado com sucesso'
+
+  end
 end
---
+++ spec/system/user_register_warehouse_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'usuario cadastra um galpão' do
+  it 'a partir da tela inicial'do
+    # Arrange
+    # Action
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    # Assert
+    expect(page).to have_field('Nome')
+    expect(page).to have_field('Código')
+    expect(page).to have_field('Cidade')
+    expect(page).to have_field('Endereço')
+    expect(page).to have_field('CEP')
+    expect(page).to have_field('Área')
+    expect(page).to have_field('Descrição')
+
+  end
+  it 'com sucesso' do
+    # Arrange
+
+    # Action
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    fill_in 'Nome', with: 'Rio de Janeiro'
+    fill_in 'Código', with: 'RIO'
+    fill_in 'Descrição', with: 'Galpão  da zona portúario do Rio'
+    fill_in 'Endereço', with: 'Avenida do Museo do Amanhã, 1000'
+    fill_in 'CEP', with: '20100-0001'
+    fill_in 'Área', with: '32000'
+    click_on 'Enviar'
+  
+    # Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Galpão cadastrado com sucesso'
+    expect(page).to have_content 'Rio de Janeiro'
+    expect(page).to have_content 'Rio'
+    expect(page).to have_content '32000 m²'
+  end
+end
