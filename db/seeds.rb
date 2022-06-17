570b3c410d7a6596176d8fc36d1b48e41e72f2e3
Vanessa <machado.vanessa32@gmail.com>
2022-06-16 23:35:10 -0300

    Adicionei galpões no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#Galpões cadastrados.
+Warehouse.create!(name: 'Aeroporto SP',
+                  code: 'GRU', 
+                  city: 'Guarulhos',
+                  area: '100_000',
+                  address: 'Avenida do Aeroporto, 1000',
+                  cep: '15000-000',
+                  description: 'Galpão destinado para cargas internacionais'
+                  )
+
+Warehouse.create(name: 'Rio', 
+                 code: 'SDU', 
+                 city: 'Rio de Janeiro', 
+                 area: 60_000,
+                 address: 'Avenida do Porto, 3000', 
+                 cep:'2000-000', 
+                 description: 'Galpão no Rio'
+                )
+
+Warehouse.create(name: 'Maceio', 
+                 code: 'MCZ', 
+                 city: 'Maceio', 
+                 area: 50_000,
+                 address: 'Avenida da Luz, 2000', 
+                 cep:'5000-000',
+                 description: 'Galpão em Maceio'
+                )
