11fbe6a89a9569497d55290adfa105f6dd29ae48
vanessa <machado.vanessa32@gmail.com>
2022-07-04 11:13:03 -0300

    Usuario faz logout

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -16,8 +16,25 @@ describe 'Usuario se autentica' do
     expect(page).to have_content 'Login efetuado com sucesso.' 
     within ('nav') do
     expect(page).not_to have_link 'Entrar'
-    expect(page).to have_link 'Sair'
+    expect(page).to have_button 'Sair'
     expect(page).to have_content 'email@example.com'
     end
+  end
+  it 'e faz logout' do
+    # Arrange
+    User.create!(email: 'email@example.com', password: '123456')
+    # Action
+    visit root_path
+    click_on 'Acessar'
+    fill_in 'e-mail', with: 'email@example.com'
+    fill_in 'Senha', with: '123456'
+    click_on 'Acessar'
+    click_on 'Sair'
+    # Assert
+    save_page
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Acessar'
+    expect(page).not_to have_button 'Sair'
+    expect(page).not_to have_content 'email@example.com'
   end
 end
